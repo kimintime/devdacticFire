56d7027b4f42ac508669fef5fb278ea8d963c26e
Apollostowel <apollostowel@icloud.com>
2022-10-28 13:04:25 +0300

    Initial Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'notepad-f29f4',
+    appId: '1:403328062821:web:4beb0a402a8eb1413dbd8c',
+    storageBucket: 'notepad-f29f4.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyARGp5Pccf1byKycnoZ3FikhylwV95CfkM',
+    authDomain: 'notepad-f29f4.firebaseapp.com',
+    messagingSenderId: '403328062821',
+    measurementId: 'G-LPRZX2W4L0',
+  },
   production: false
 };
 
